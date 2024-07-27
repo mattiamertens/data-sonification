ab231c1e016c7bdedc60246d4e27321e29a686d2
Mattia Mertens <mattianc@hotmail.it>
2024-07-27 19:50:54 +0200

    about section :,(

--- bio.js
+++ bio.js
@@ -11,9 +11,12 @@ const pic_dots = d3.select(".dots")
 
 d3.json("assets/bio.json").then(dataDots =>{
     updateDots(dataDots);
+    $('.cerchio').first().addClass('active');
 })
 
 function updateDots(dataDots){
+
+    
 
     // const node = pic_dots.append("div")
     //     .attr("class", "cerchio")
@@ -27,9 +30,10 @@ function updateDots(dataDots){
         .enter().append("div")
         .attr("class", "cerchio")
         .on("click", function(event, d){
-            console.log(d);
             $('.about-img').attr('src', 'assets/pics/resized/'+d.Foto+'.jpg');
             $('.bio-text')[0].innerHTML = d.Bio;
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
         });
 
 }
